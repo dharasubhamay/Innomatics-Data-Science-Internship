bd5fbcbd9d3533870bf805e02811b2c11b186a7c
dharasubhamay <82009535+dharasubhamay@users.noreply.github.com>
2023-04-22 11:01:15 +0530

    Add files via upload

---
+++ FindTheNearestPub/app.py
@@ -0,0 +1,25 @@
+import streamlit as st
+import pandas as pd
+import os
+
+# absolute path to this file
+FILE_DIR = os.path.dirname(os.path.abspath(__file__))
+# absolute path of directory_of_interest
+dir_of_interest = os.path.join(FILE_DIR, "data")
+DATA_PATH = os.path.join(dir_of_interest, "open_pubs.csv")
+df=pd.read_csv(DATA_PATH)
+
+st.set_page_config(page_title="Home")
+
+st.title("Open Nearest Pub")
+st.subheader('Dataset')
+st.dataframe(df)
+
+st.subheader('Covariance in dataset')
+st.dataframe(df.cov())
+
+st.subheader('Satistical Analysis')    
+st.dataframe(df.describe())
+
+st.subheader('Correlation')  
+st.dataframe(df.corr())
